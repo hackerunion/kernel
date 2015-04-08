c6b77002f88a2d13f6c5cceb021ac8102795c9ca
Brandon Diamond <brandon.t.diamond@gmail.com>
2015-04-07 23:42:20 -0400

    =

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: app.get('cookie secret') + ':' + process.pid,
+  secret: app.get('cookie secret'),
   signed: true
 }));
 
