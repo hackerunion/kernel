54e6a8181e432b13e6543b4f484aa54c2ca0947e
Brandon Diamond <brandon.t.diamond@gmail.com>
2015-07-20 22:14:11 -0400

    +

--- app.js
+++ app.js
@@ -234,7 +234,10 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    if (!res.headersSent) {
+      res.status(err.status || 500);
+    }
+
     res.render('error', {
         message: err.message,
         error: app.get('production') ? {} : err
